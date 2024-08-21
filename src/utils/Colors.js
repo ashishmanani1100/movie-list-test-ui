5a740368332d3dd5b4cfb37052b1e09853ebd634
ashishmanani1100 <109693670+ashishmanani1100@users.noreply.github.com>
2024-08-21 18:49:26 +0530

    Delete src/utils directory

--- src/utils/Colors.js
+++
@@ -1,9 +0,0 @@
-// Defines colors used in app
-
-export const COLOR_BLUE = "#093545";
-export const COLOR_RED = "#EB5757";
-export const COLOR_BLUE_LIGHT = "#224957";
-export const COLOR_WHITE = "#FFF";
-export const COLOR_GREEN = "#2BD17E";
-export const COLOR_DARK_BLUE = "#092C39";
-export const COLOR_LIGHT_BLUE = "#0829358C";
